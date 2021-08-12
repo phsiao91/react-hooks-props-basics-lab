02fb4cfefd5054ba33a126159a1aa7245ee6a9f3
Phing An Hsiao <phinganhsiao@gmail.com>
2021-08-12 14:13:37 -0400

    commit tree 3be9f42ce905be73e77ac6a1341fa515e025cb37

--- src/data/user.js
+++ src/data/user.js
@@ -1,9 +1,10 @@
 const user = {
+  
   name: "Liza",
   city: "New York",
   bio: "I made this!",
   color: "firebrick",
-  links: {
+  links :{
     github: "https://github.com/liza",
     linkedin: "https://www.linkedin.com/in/liza/",
   },
